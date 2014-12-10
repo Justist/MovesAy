88825e22f6aaada0e4a8b6365907db176dc3d101
Justist <skl_oh2@hotmail.com>
2014-12-10 17:38:25 +0100

    Speech recognition implemented in Snake sample
    Still needs rework to make it an acceptable submission, but we are
    getting there. At least the input is now processed into something
    visual.

--- app/src/main/java/movesay/hci/team8/movesay/GameActivity.java
+++ app/src/main/java/movesay/hci/team8/movesay/GameActivity.java
@@ -8,22 +8,49 @@ import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.support.annotation.NonNull;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import movesay.hci.team8.movesay.util.SnakeView;
+
+
 public class GameActivity extends Activity {
-	private String textMatch;
+	public TextView textMatch;
 
 	private SpeechRecognizer mSpeechRecognizer;
 	private Intent mSpeechRecognizerIntent;
+
+	private SnakeView mSnakeView;
+	private static String ICICLE_KEY = "snakeview";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.activity_game);
+		setContentView(R.layout.snake_layout);
+		//setContentView(R.layout.activity_game);
+
+		textMatch = (TextView) findViewById(R.id.result);
+
+		mSnakeView = (SnakeView) findViewById(R.id.snake);
+		mSnakeView.setTextView((TextView) findViewById(R.id.text));
+
+		if (savedInstanceState == null) {
+			// We were just launched -- set up a new game
+			mSnakeView.setMode(SnakeView.READY);
+		} else {
+			// We are being restored
+			Bundle map = savedInstanceState.getBundle(ICICLE_KEY);
+			if (map != null) {
+				mSnakeView.restoreState(map);
+			} else {
+				mSnakeView.setMode(SnakeView.PAUSE);
+			}
+		}
 
 		mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
 		mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -35,20 +62,33 @@ public class GameActivity extends Activity {
 		if(checkVoiceRecognition()) {mSpeechRecognizer.startListening(mSpeechRecognizerIntent);}
     }
 
-	void showToastMessage(String message){
-		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-	}
-
 	public boolean checkVoiceRecognition() {
 		// Check if voice recognition is present
 		PackageManager pm = getPackageManager();
 		List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(
 			RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
 		if (activities.size() == 0) {
-			Toast.makeText(this, "Voice recognizer not present", Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, "Voice recognizer not present", Toast.LENGTH_LONG).show();
 			return false;
 		}
 		return true;
+	}
+
+	public void showToastMessage(String message){
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// Pause the game along with the activity
+		mSnakeView.setMode(SnakeView.PAUSE);
+	}
+
+	@Override
+	public void onSaveInstanceState(@NonNull Bundle outState) {
+		//Store the game state
+		outState.putBundle(ICICLE_KEY, mSnakeView.saveState());
 	}
 
 	protected class SpeechRecognitionListener implements RecognitionListener {
@@ -69,7 +109,7 @@ public class GameActivity extends Activity {
 
 		@Override
 		public void onError(int error) {
-			//showToastMessage("error" + error);
+			//textMatch.setText("Nope, I did not get that...");
 			mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
 		}
 
@@ -77,8 +117,8 @@ public class GameActivity extends Activity {
 		public void onResults(Bundle results) {
 			ArrayList<String> textMatchList = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
 			if (!textMatchList.isEmpty()) {
-				textMatch = textMatchList.get(0);
-				showToastMessage("You said: "+ textMatch);
+				mSnakeView.recognizeDirection(textMatchList.get(0).toLowerCase());
+				//showToastMessage(textMatchList.get(0).toLowerCase());
 			}
 			mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
 		}
